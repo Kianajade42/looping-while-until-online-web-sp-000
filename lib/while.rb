8cf7c9dabdb1d423f1e79fa13955ad6430938b30
Kiana Long <kianajade42.kl@gmail.com>
2021-01-12 14:19:55 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,9 +2,3 @@ def using_while
   levitation_force = 6
   puts "Wingardium Leviosa"
 end
-loop
-  levitation_force += 1
-  until
-    levitation_force == 10
-      puts "Wingardium Leviosa"
-    end
