3a91344f283b01cd451d2023aabdce539cc45c8e
Kiana Long <kianajade42.kl@gmail.com>
2021-01-12 14:18:37 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,7 @@
 def using_until
   levitation_force = 6
-  do loop
-    Wingardium Leviosa
+  loop  levitation_force += 1
+  until levitation_force == 10
+    puts "Wingardium Leviosa"
+    end
   end
-end
